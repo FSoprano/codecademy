6927e7a328c763ad3a6f10b3dae314044de47a13
KUESTER <kuester@de.ibm.com>
2019-08-19 19:04:53 +0200

    Add issues post route

--- capstone-project-1-x-press-publishing/api/issues.js
+++ capstone-project-1-x-press-publishing/api/issues.js
@@ -13,4 +13,39 @@ issuesRouter.get('/', (req, res, next) => {
     });
 });
 
+const checkIssueFields = (req, res, next) => {
+    const objectToCheck = req.body.issue;
+    console.log(req.params);
+    // console.log(req.body.seriesId);
+    const idquery = db.get(`SELECT * FROM Issue WHERE artist_id = ${objectToCheck.artistId}`);
+    if (!objectToCheck.name || !objectToCheck.issueNumber || !objectToCheck.publicationDate || !objectToCheck.artistId) {
+      return res.sendStatus(400);
+    } else if (idquery === null) {
+        return res.sendStatus(400);
+    }
+    next();
+  }
+
+issuesRouter.post('/', checkIssueFields, (req, res, next) => {
+    
+    
+    db.run(`INSERT INTO Issue (name, issue_number, publication_date, artist_id, series_id) VALUES 
+    ($name, $issueNumber, $publicationDate, $artistId, $seriesId)`, {
+        $name: req.body.issue.name,
+        $issueNumber: req.body.issue.issueNumber,
+        $publicationDate: req.body.issue.publicationDate,
+        $artistId: req.body.issue.artistId,
+        $seriesId: req.params.seriesId
+    }, function (err) {
+        if (err) {
+          next(err);
+        } 
+        db.get(`SELECT * FROM Issue WHERE Issue.id = ${this.lastID}`, 
+                                     function (err, row) {
+              
+               res.status(201).json({issue: row});     
+            });
+        });
+  });
+
 module.exports = issuesRouter;
