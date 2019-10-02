effe1a5f636e2277bbf2a59ba9b3b2b43fb612d2
KUESTER <kuester@de.ibm.com>
2019-10-02 15:50:01 +0200

    Expresso project: initial commit

---
+++ capstone-project-2-expresso-start/api/employees.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const empRouter = express.Router();
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+empRouter.get('/', (req, res, next) => {
+    db.all('SELECT * FROM Employee WHERE is_current_employee = 1', function (err, rows) {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200).json({employees: rows});
+        }
+    });
+});
+
+module.exports = empRouter;
---
+++ capstone-project-2-expresso-start/api/menus.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const menuRouter = express.Router();
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+menuRouter.get('/', (req, res, next) => {
+    db.all('SELECT * FROM Menu', function (err, rows) {
+        if (err) {
+            next(err);
+        } else {
+            res.status(200);
+        }
+    });
+});
+
+module.exports = menuRouter;
